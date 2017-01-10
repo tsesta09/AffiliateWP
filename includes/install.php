05620b541a8209b9d8cabd6919ded9567563e157
Drew Jaynes <drew@affiliatewp.com>
2017-01-10 16:45:15 -0700

    Revert "Make sure we bail from affiliate_wp_install() if affwp_is_installed is true."
    This reverts commit 2c86f6e805be37f5648b1b37600a7a5e2d30a881.

--- includes/install.php
+++ includes/install.php
@@ -2,11 +2,6 @@
 
 function affiliate_wp_install() {
 
-	// Bail if AffiliateWP is already installed.
-	if( ! get_option( 'affwp_is_installed' ) ) {
-		return;
-	}
-
 	// Create affiliate caps
 	$roles = new Affiliate_WP_Capabilities;
 	$roles->add_caps();
