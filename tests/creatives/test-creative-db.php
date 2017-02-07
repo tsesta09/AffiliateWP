9590562f515f54dff3e7f530cac595843c30f73b
Drew Jaynes <drew@affiliatewp.com>
2017-02-07 16:16:43 -0700

    Bring over two tests for creatives from issue/1175/creatives-db.

--- tests/creatives/test-creative-db.php
+++ tests/creatives/test-creative-db.php
@@ -29,6 +29,26 @@ class Tests extends UnitTestCase {
 	}
 
 	/**
+	 * @covers Affiliate_WP_Creatives_DB::__construct()
+	 */
+	public function test_creatives_network_wide_table_name_should_be_affiliate_wp_creatives() {
+		if ( defined( 'AFFILIATE_WP_NETWORK_WIDE' ) && AFFILIATE_WP_NETWORK_WIDE ) {
+			$this->assertEquals( 'affiliate_wp_creatives', affiliate_wp()->creatives->table_name );
+		}
+	}
+
+	/**
+	 * @covers Affiliate_WP_Creatives_DB::__construct()
+	 */
+	public function test_creatives_not_network_wide_table_name_should_be_prefix_affiliate_wp_creatives() {
+		if ( ! defined( 'AFFILIATE_WP_NETWORK_WIDE' ) ) {
+			global $wpdb;
+
+			$this->assertEquals( $wpdb->prefix . 'affiliate_wp_creatives', affiliate_wp()->creatives->table_name );
+		}
+	}
+
+	/**
 	 * @covers \Affiliate_WP_Creatives_DB::$cache_group
 	 */
 	public function test_cache_group_should_be_creatives() {
